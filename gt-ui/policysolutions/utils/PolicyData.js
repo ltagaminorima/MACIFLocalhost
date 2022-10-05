9235757812252dd134e05271199247b141065b95
Lloyd Tagami <ltagami@guidewire.com>
2022-10-05 12:50:06 +0800

    added utils folder

---
+++ gt-ui/policysolutions/utils/PolicyData.js
@@ -0,0 +1,33 @@
+module.exports = {
+    AccountforPersonalAuto: {
+        accountNumber: "5796277369",
+        accountName: ""
+    },
+    Antecedentsconcurrence: "No", 
+    VehiculeParticulierSubmissionDetails: {        
+        Immatriculation: "Test Vehicle",
+        Marque: "PEUGEOT",
+        Annee: "2020",
+        Modele: "RIFTER",
+        Financement: "Crédit (même partiel)",
+        PaysdeProvence: "France",
+        LicenseType: "B (Ang)",
+        LicenseDate: "01/01/2015",
+        LicenseLearningMode: "Standard",
+        ActiveConducteur: "Autres",
+        UsageofVehicle: "Uniquement privé",
+        LicenseInvalidation: "No",
+        LicenseTermination: "No",
+        DriverHistoryConducteurPrincipal: "Aucun antécédent",
+        DriverHistoryNoviceNeuralization: "Aucune",
+        DriverHistoryAccident: "0",
+        DriverHistoryTheft: "0",
+        DriverHistoryBreakage: "0",
+        DriverHistoryCoefficient: "100",
+        DriverHistoryNoInsuranceDays: "365",        
+    },
+    CoverageOfferingSelection: "Economique (ang)",
+    ConditionFrequementPayment: "Annuel",
+    SubmissionNumber: "",
+    RiskAnalysisReason: "For Testing"
+};
